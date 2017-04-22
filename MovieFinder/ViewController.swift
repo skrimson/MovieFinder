db9955e74897fa0b137245766e35048a568a6296
KimSeunghuk <skrimm00ak@gmail.com>
2017-04-22 17:34:17 +0900

    Initial Commit

---
+++ MovieFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieFinder
+//
+//  Created by KimSeunghuk on 2017/04/22.
+//  Copyright © 2017 SkrimmApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
